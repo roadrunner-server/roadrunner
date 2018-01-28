7fa0ea8e9b18697fe06673ba9c5f24f32b06afa2
Wolfy-J <wolfy.jd@gmail.com>
2018-01-28 19:13:04 +0300

    protocol tests

---
+++ protocol_test.go
@@ -0,0 +1,46 @@
+package roadrunner
+
+import (
+	"testing"
+	"github.com/spiral/goridge"
+	"github.com/stretchr/testify/assert"
+	"github.com/pkg/errors"
+)
+
+type relayMock struct {
+	error   bool
+	payload string
+}
+
+func (r *relayMock) Send(data []byte, flags byte) (err error) {
+	if r.error {
+		return errors.New("send error")
+	} else {
+		return nil
+	}
+}
+
+func (r *relayMock) Receive() (data []byte, p goridge.Prefix, err error) {
+	return []byte(r.payload), goridge.NewPrefix().WithFlag(goridge.PayloadControl), nil
+}
+
+func (r *relayMock) Close() error {
+	return nil
+}
+
+func Test_Protocol_Errors(t *testing.T) {
+	err := sendPayload(&relayMock{}, make(chan int))
+	assert.Error(t, err)
+}
+
+func Test_Protocol_FetchPID(t *testing.T) {
+	pid, err := fetchPID(&relayMock{error: false, payload: "{\"pid\":100}"})
+	assert.NoError(t, err)
+	assert.Equal(t, 100, pid)
+
+	_, err = fetchPID(&relayMock{error: true, payload: "{\"pid\":100}"})
+	assert.Error(t, err)
+
+	_, err = fetchPID(&relayMock{error: false, payload: "{\"pid:100"})
+	assert.Error(t, err)
+}
