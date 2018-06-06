f688b45aa74378287d12a8a0197aebbb7b5499c9
Wolfy-J <wolfy.jd@gmail.com>
2018-06-06 16:07:30 +0300

    better server re-configuration

--- server_config_test.go
+++ server_config_test.go
@@ -24,7 +24,7 @@ func Test_ServerConfig_PipeFactory(t *testing.T) {
 }
 
 func Test_ServerConfig_SocketFactory(t *testing.T) {
-	cfg := &ServerConfig{Relay: "tcp://:9000"}
+	cfg := &ServerConfig{Relay: "tcp://:9111"}
 	f, err := cfg.makeFactory()
 	defer f.Close()
 
@@ -33,14 +33,15 @@ func Test_ServerConfig_SocketFactory(t *testing.T) {
 	assert.Equal(t, "tcp", f.(*SocketFactory).ls.Addr().Network(), )
 	assert.Equal(t, "[::]:9000", f.(*SocketFactory).ls.Addr().String())
 
-	cfg = &ServerConfig{Relay: "tcp://localhost:9000"}
+	cfg = &ServerConfig{Relay: "tcp://localhost:9111"}
 	f, err = cfg.makeFactory()
+	assert.NoError(t, err)
 	defer f.Close()
 
 	assert.NoError(t, err)
 	assert.IsType(t, &SocketFactory{}, f)
 	assert.Equal(t, "tcp", f.(*SocketFactory).ls.Addr().Network())
-	assert.Equal(t, "127.0.0.1:9000", f.(*SocketFactory).ls.Addr().String())
+	assert.Equal(t, "127.0.0.1:9111", f.(*SocketFactory).ls.Addr().String())
 }
 
 func Test_ServerConfig_UnixSocketFactory(t *testing.T) {
