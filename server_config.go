dc56d924ac34252f1866dd067a9e80c30b0d133c
Wolfy-J <wolfy.jd@gmail.com>
2018-06-10 13:14:44 +0300

    working with more tests and isolated command

--- server_config.go
+++ server_config.go
@@ -5,15 +5,15 @@ import (
 	"net"
 	"strings"
 	"time"
-)
-
-const (
-	FactoryPipes = iota
-	FactorySocket
+	"os/exec"
 )
 
 // Server config combines factory, pool and cmd configurations.
 type ServerConfig struct {
+	// Command includes command strings with all the parameters, example: "php worker.php pipes". This config section
+	// must not change on re-configuration.
+	Command string
+
 	// Relay defines connection method and factory to be used to connect to workers:
 	// "pipes", "tcp://:6001", "unix://rr.sock"
 	// This config section must not change on re-configuration.
@@ -26,6 +26,19 @@ type ServerConfig struct {
 	// Pool defines worker pool configuration, number of workers, timeouts and etc. This config section might change
 	// while server is running.
 	Pool Config
+}
+
+// Differs returns true if configuration has changed but ignores pool changes.
+func (cfg *ServerConfig) Differs(new *ServerConfig) bool {
+	return cfg.Command != new.Command || cfg.Relay != new.Relay || cfg.RelayTimeout != new.RelayTimeout
+}
+
+// makeCommands returns new command provider based on configured options.
+func (cfg *ServerConfig) makeCommand() (func() *exec.Cmd, error) {
+	var cmd = strings.Split(cfg.Command, " ")
+	return func() *exec.Cmd {
+		return exec.Command(cmd[0], cmd[1:]...)
+	}, nil
 }
 
 // makeFactory creates and connects new factory instance based on given parameters.
