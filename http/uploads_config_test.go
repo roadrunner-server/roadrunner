ba99a17a3a7bb88d44fdc6e65001ef0b0dcd6833
Wolfy-J <wolfy.jd@gmail.com>
2018-06-10 18:34:01 +0300

    fixing some tests

--- http/uploads_config_test.go
+++ http/uploads_config_test.go
@@ -3,6 +3,7 @@ package http
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"os"
 )
 
 func TestFsConfig_Forbids(t *testing.T) {
@@ -12,4 +13,12 @@ func TestFsConfig_Forbids(t *testing.T) {
 	assert.True(t, cfg.Forbids("index.PHP"))
 	assert.True(t, cfg.Forbids("phpadmin/index.bak.php"))
 	assert.False(t, cfg.Forbids("index.html"))
+}
+
+func TestFsConfig_TmpFallback(t *testing.T) {
+	cfg := UploadsConfig{Dir: "test"}
+	assert.Equal(t, "test", cfg.TmpDir())
+
+	cfg = UploadsConfig{Dir: ""}
+	assert.Equal(t, os.TempDir(), cfg.TmpDir())
 }
