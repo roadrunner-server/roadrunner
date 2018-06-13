9942d1a8b2163fc9a6fc1377a0760512d960502a
Wolfy-J <wolfy.jd@gmail.com>
2018-06-13 22:39:25 +0300

    why php

---
+++ php-src/tests/http/error2.php
@@ -0,0 +1,9 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+function handleRequest(ServerRequestInterface $req, ResponseInterface $resp): ResponseInterface
+{
+    exit();
+}
