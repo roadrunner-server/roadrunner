a47fdb8b8c93a8e2c9af1d786e746b145f4b16ed
Wolfy-J <wolfy.jd@gmail.com>
2018-06-23 20:35:37 +0300

    custom version

--- cmd/rr/cmd/version.go
+++ cmd/rr/cmd/version.go
@@ -1,8 +1,10 @@
 package cmd
 
+import "time"
+
 var (
 	// Version - defines build version.
-	Version = "1.0.0"
+	Version = "development"
 	// BuildTime - defined build time.
-	BuildTime = "development"
+	BuildTime = time.Now()
 )
