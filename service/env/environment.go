ad1b967117f04772fcdbabc6356bf12db90340dc
Wolfy-J <wolfy.jd@gmail.com>
2018-09-10 15:49:40 +0300

    Update environment.go

--- service/env/environment.go
+++ service/env/environment.go
@@ -1,6 +1,6 @@
 package env
 
-// Provider aggregates list of environment variables. This interface can be used in custom implementation to drive
+// Environment aggregates list of environment variables. This interface can be used in custom implementation to drive
 // values from external sources.
 type Environment interface {
 	// GetEnv must return list of env variables.
