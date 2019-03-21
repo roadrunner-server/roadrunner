c5f8f9d12de48d93db2c9ef63acc5bfab4f1d08a
Wolfy-J <wolfy.jd@gmail.com>
2019-03-21 14:50:32 +0300

    version bump

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd $(dirname "${BASH_SOURCE[0]}")
 OD="$(pwd)"
 # Pushes application version into the build information.
-RR_VERSION=1.3.5
+RR_VERSION=1.3.6
 
 # Hardcode some values to the core package
 LDFLAGS="$LDFLAGS -X github.com/spiral/roadrunner/cmd/rr/cmd.Version=${RR_VERSION}"
