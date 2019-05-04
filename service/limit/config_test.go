2fb995876b208dc40912d11764262e33920dd4fa
Wolfy-J <wolfy.jd@gmail.com>
2019-05-04 23:08:28 +0300

    golint

--- service/limit/config_test.go
+++ service/limit/config_test.go
@@ -13,7 +13,7 @@ func (cfg *mockCfg) Get(name string) service.Config  { return nil }
 func (cfg *mockCfg) Unmarshal(out interface{}) error { return json.Unmarshal([]byte(cfg.cfg), out) }
 
 func Test_Config_Hydrate_Error1(t *testing.T) {
-	cfg := &mockCfg{`{"enable": true}`}
+	cfg := &mockCfg{`{"enable: true}`}
 	c := &Config{}
 
 	assert.Error(t, c.Hydrate(cfg))
