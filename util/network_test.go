d5b8235cbe7e7e1f48b788accbc39d54c0977aa5
Wolfy-J <wolfy.jd@gmail.com>
2019-06-24 14:05:45 +0300

    more tests

--- util/network_test.go
+++ util/network_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/stretchr/testify/assert"
+	"runtime"
 	"testing"
 )
 
@@ -11,4 +12,14 @@ func TestCreateListener(t *testing.T) {
 
 	_, err = CreateListener("aaa://192.168.0.1")
 	assert.Error(t, err, "Invalid Protocol (tcp://:6001, unix://file.sock)")
+}
+
+func TestUnixCreateListener(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("not supported on " + runtime.GOOS)
+	}
+
+	l, err := CreateListener("unix://file.sock")
+	assert.NoError(t, err)
+	l.Close()
 }
