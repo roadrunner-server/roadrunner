0ffb62f682721637370f2c9f0e5480a69eb5dc4b
Wolfy-J <wolfy.jd@gmail.com>
2019-06-27 14:09:08 +0300

    more tests

--- service/metrics/rpc_test.go
+++ service/metrics/rpc_test.go
@@ -192,3 +192,274 @@ func Test_Set_RPC_MetricError(t *testing.T) {
 		Labels: []string{"missing"},
 	}, &ok))
 }
+
+func Test_Set_RPC_MetricError_2(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(rpc.ID, &rpc.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		rpcCfg: `{"enable":true, "listen":"tcp://:5004"}`,
+		metricsCfg: `{
+		"address": "localhost:2112",
+		"collect":{
+			"user_gauge":{
+				"type": "gauge",
+				"labels": ["type", "section"]
+			}
+		}
+
+	}`}))
+
+	s, _ := c.Get(ID)
+	assert.NotNil(t, s)
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	assert.True(t, s.(*Service).Enabled())
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	client, err := rs.Client()
+	assert.NoError(t, err)
+
+	var ok bool
+	assert.Error(t, client.Call("metrics.Set", Metric{
+		Name:  "user_gauge",
+		Value: 100.0,
+	}, &ok))
+}
+
+// -- observe
+
+func Test_Observe_RPC(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(rpc.ID, &rpc.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		rpcCfg: `{"enable":true, "listen":"tcp://:5004"}`,
+		metricsCfg: `{
+		"address": "localhost:2112",
+		"collect":{
+			"user_histogram":{
+				"type": "histogram"
+			}
+		}
+
+	}`}))
+
+	s, _ := c.Get(ID)
+	assert.NotNil(t, s)
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	assert.True(t, s.(*Service).Enabled())
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	client, err := rs.Client()
+	assert.NoError(t, err)
+
+	var ok bool
+	assert.NoError(t, client.Call("metrics.Observe", Metric{
+		Name:  "user_histogram",
+		Value: 100.0,
+	}, &ok))
+	assert.True(t, ok)
+
+	out, _, err := get("http://localhost:2112/metrics")
+	assert.NoError(t, err)
+	assert.Contains(t, out, `user_histogram`)
+}
+
+func Test_Observe_RPC_Vector(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(rpc.ID, &rpc.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		rpcCfg: `{"enable":true, "listen":"tcp://:5004"}`,
+		metricsCfg: `{
+		"address": "localhost:2112",
+		"collect":{
+			"user_histogram":{
+				"type": "histogram",
+				"labels": ["type", "section"]
+			}
+		}
+
+	}`}))
+
+	s, _ := c.Get(ID)
+	assert.NotNil(t, s)
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	assert.True(t, s.(*Service).Enabled())
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	client, err := rs.Client()
+	assert.NoError(t, err)
+
+	var ok bool
+	assert.NoError(t, client.Call("metrics.Observe", Metric{
+		Name:   "user_histogram",
+		Value:  100.0,
+		Labels: []string{"core", "first"},
+	}, &ok))
+	assert.True(t, ok)
+
+	out, _, err := get("http://localhost:2112/metrics")
+	assert.NoError(t, err)
+	assert.Contains(t, out, `user_histogram`)
+}
+
+func Test_Observe_RPC_CollectorError(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(rpc.ID, &rpc.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		rpcCfg: `{"enable":true, "listen":"tcp://:5004"}`,
+		metricsCfg: `{
+		"address": "localhost:2112",
+		"collect":{
+			"user_histogram":{
+				"type": "histogram",
+				"labels": ["type", "section"]
+			}
+		}
+	}`}))
+
+	s, _ := c.Get(ID)
+	assert.NotNil(t, s)
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	assert.True(t, s.(*Service).Enabled())
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	client, err := rs.Client()
+	assert.NoError(t, err)
+
+	var ok bool
+	assert.Error(t, client.Call("metrics.Observe", Metric{
+		Name:   "user_histogram",
+		Value:  100.0,
+		Labels: []string{"missing"},
+	}, &ok))
+}
+
+func Test_Observe_RPC_MetricError(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(rpc.ID, &rpc.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		rpcCfg: `{"enable":true, "listen":"tcp://:5004"}`,
+		metricsCfg: `{
+		"address": "localhost:2112",
+		"collect":{
+			"user_histogram":{
+				"type": "histogram",
+				"labels": ["type", "section"]
+			}
+		}
+
+	}`}))
+
+	s, _ := c.Get(ID)
+	assert.NotNil(t, s)
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	assert.True(t, s.(*Service).Enabled())
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	client, err := rs.Client()
+	assert.NoError(t, err)
+
+	var ok bool
+	assert.Error(t, client.Call("metrics.Observe", Metric{
+		Name:   "user_histogram",
+		Value:  100.0,
+		Labels: []string{"missing"},
+	}, &ok))
+}
+
+func Test_Observe_RPC_MetricError_2(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(rpc.ID, &rpc.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		rpcCfg: `{"enable":true, "listen":"tcp://:5004"}`,
+		metricsCfg: `{
+		"address": "localhost:2112",
+		"collect":{
+			"user_histogram":{
+				"type": "histogram",
+				"labels": ["type", "section"]
+			}
+		}
+
+	}`}))
+
+	s, _ := c.Get(ID)
+	assert.NotNil(t, s)
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	assert.True(t, s.(*Service).Enabled())
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	client, err := rs.Client()
+	assert.NoError(t, err)
+
+	var ok bool
+	assert.Error(t, client.Call("metrics.Observe", Metric{
+		Name:  "user_histogram",
+		Value: 100.0,
+	}, &ok))
+}
