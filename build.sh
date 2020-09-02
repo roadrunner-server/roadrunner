163c72df6e4a46aa78ab825670aedce069bae40f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-02 11:30:46 +0300

    release 1.8.3

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd $(dirname "${BASH_SOURCE[0]}")
 OD="$(pwd)"
 
 # Pushes application version into the build information.
-RR_VERSION=1.8.2
+RR_VERSION=1.8.3
 
 # Hardcode some values to the core package
 LDFLAGS="$LDFLAGS -X github.com/spiral/roadrunner/cmd/rr/cmd.Version=${RR_VERSION}"
