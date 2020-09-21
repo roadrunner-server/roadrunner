bc8a3ff724c425a31c2c50a7b5f093983ed792ef
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-21 12:41:53 +0300

    Simplify watcher test, fix lgtm warinings and errors

--- worker.go
+++ worker.go
@@ -250,7 +250,7 @@ func (w *Worker) execPayload(rqs *Payload) (rsp *Payload, err error) {
 	}
 
 	// add streaming support :)
-	if rsp.Body, pr, err = w.rl.Receive(); err != nil {
+	if rsp.Body, _, err = w.rl.Receive(); err != nil {
 		return nil, errors.Wrap(err, "worker error")
 	}
 
