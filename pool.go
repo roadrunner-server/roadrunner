6f39542d75d0da1e0ff09906bdd340f855a409af
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-19 14:06:26 +0300

    Add some comments

--- pool.go
+++ pool.go
@@ -48,6 +48,7 @@ type Pool interface {
 	// Exec one task with given payload and context, returns result or error.
 	ExecWithContext(ctx context.Context, rqs Payload) (Payload, error)
 
+	// Exec 
 	Exec(rqs Payload) (Payload, error)
 
 	// Workers returns worker list associated with the pool.
