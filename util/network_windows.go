91c268032d030241108147fa40a9f509cfbb5869
Leo <hardrain980@protonmail.com>
2020-11-25 12:08:59 +0800

    Default listen addresses without protocol prefix ('tcp://' or 'unix://') as TCP.

--- util/network_windows.go
+++ util/network_windows.go
@@ -14,22 +14,32 @@ import (
 // CreateListener crates socket listener based on DSN definition.
 func CreateListener(address string) (net.Listener, error) {
 	dsn := strings.Split(address, "://")
-	if len(dsn) != 2 {
-		return nil, errors.New("invalid DSN (tcp://:6001, unix://file.sock)")
+
+	var protocol string
+	var param    string
+
+	if len(dsn) == 1 {
+		protocol = "tcp"
+		param = dsn[0]
+	} else if len(dsn) == 2 {
+		if dsn[0] != "unix" && dsn[0] != "tcp" {
+			return nil, errors.New("invalid Protocol ([tcp://]:6001, unix://file.sock)")
+		} else {
+			protocol = dsn[0]
+			param = dsn[1]
+		}
+	} else {
+		return nil, errors.New("invalid DSN ([tcp://]:6001, unix://file.sock)")
 	}
 
-	if dsn[0] != "unix" && dsn[0] != "tcp" {
-		return nil, errors.New("invalid Protocol (tcp://:6001, unix://file.sock)")
-	}
-
-	if dsn[0] == "unix" && fileExists(dsn[1]) {
-		err := syscall.Unlink(dsn[1])
+	if protocol == "unix" && fileExists(param) {
+		err := syscall.Unlink(param)
 		if err != nil {
 			return nil, fmt.Errorf("error during the unlink syscall: error %v", err)
 		}
 	}
 
-	return net.Listen(dsn[0], dsn[1])
+	return net.Listen(protocol, param)
 }
 
 // fileExists checks if a file exists and is not a directory before we
