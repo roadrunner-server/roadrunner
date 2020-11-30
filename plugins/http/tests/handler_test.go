b9ddb1069352e36f97b1b9a4279d38e18329207a
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-30 18:20:55 +0300

    PHP 8.0 in RR2

--- plugins/http/tests/handler_test.go
+++ plugins/http/tests/handler_test.go
@@ -614,8 +614,15 @@ func TestHandler_FormData_POST_Overwrite(t *testing.T) {
 
 	form := url.Values{}
 
-	form.Add("key", "value1")
+	form.Add("key", "value")
 	form.Add("key", "value2")
+	form.Add("name[]", "name1")
+	form.Add("name[]", "name2")
+	form.Add("name[]", "name3")
+	form.Add("arr[x][y][z]", "y")
+	form.Add("arr[x][y][e]", "f")
+	form.Add("arr[c]p", "l")
+	form.Add("arr[c]z", "")
 
 	req, err := http.NewRequest("POST", "http://localhost"+hs.Addr, strings.NewReader(form.Encode()))
 	assert.NoError(t, err)
@@ -637,7 +644,7 @@ func TestHandler_FormData_POST_Overwrite(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 200, r.StatusCode)
 
-	assert.Equal(t, `{"key":"value2","arr":{"x":{"y":null}}}`, string(b))
+	assert.Equal(t, `{"arr":{"c":{"p":"l","z":""},"x":{"y":{"e":"f","z":"y"}}},"key":"value2","name":["name1","name2","name3"]}`, string(b))
 }
 
 func TestHandler_FormData_POST_Form_UrlEncoded_Charset(t *testing.T) {
