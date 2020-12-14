6ae6f8968188d5851e1929fff81e15cb730619b4
Wolfy-J <wolfy.jd@gmail.com>
2020-12-14 18:23:03 +0300

    - fixing tests

--- plugins/server/tests/tcp.php
+++ plugins/server/tests/tcp.php
@@ -6,14 +6,14 @@
 use Spiral\Goridge;
 use Spiral\RoadRunner;
 
-require dirname(__DIR__) . "/../../vendor_php/autoload.php";
+require dirname(__DIR__) . "/../../tests/vendor/autoload.php";
 
 $relay = new Goridge\SocketRelay("localhost", 9999);
 $rr = new RoadRunner\Worker($relay);
 
-while ($in = $rr->receive($ctx)) {
+while ($in = $rr->waitPayload()) {
     try {
-        $rr->send((string)$in);
+        $rr->send((string)$in->body);
     } catch (\Throwable $e) {
         $rr->error((string)$e);
     }
