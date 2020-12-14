ea25ded710988949f99eea395e31e3ac61f844f8
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-14 18:58:32 +0300

    update goridge in go.mod

--- plugins/resetter/tests/resetter_test.go
+++ plugins/resetter/tests/resetter_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInformerInit(t *testing.T) {
+func TestResetterInit(t *testing.T) {
 	cont, err := endure.NewContainer(nil, endure.SetLogLevel(endure.DebugLevel))
 	if err != nil {
 		t.Fatal(err)
@@ -53,7 +53,7 @@ func TestInformerInit(t *testing.T) {
 
 	tt := time.NewTimer(time.Second * 15)
 
-	t.Run("InformerRpcTest", resetterRPCTest)
+	t.Run("ResetterRpcTest", resetterRPCTest)
 
 	for {
 		select {
@@ -94,6 +94,7 @@ func resetterRPCTest(t *testing.T) {
 
 	var services []string
 	err = client.Call("resetter.List", nil, &services)
+	assert.NotNil(t, services)
 	assert.NoError(t, err)
 	if services[0] != "resetter.plugin1" {
 		t.Fatal("no enough services")
