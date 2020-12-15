9b1c2648bdbf80bdf43fbe2c64d06cfbad1340fc
Wolfy-J <wolfy.jd@gmail.com>
2020-12-15 11:32:49 +0300

    - fixing tests

--- tests/psr-worker.php
+++ tests/psr-worker.php
@@ -6,10 +6,15 @@ use Spiral\Goridge;
 use Spiral\RoadRunner;
 
 ini_set('display_errors', 'stderr');
-require dirname(__DIR__) . "/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 $worker = new RoadRunner\Worker(new Goridge\StreamRelay(STDIN, STDOUT));
-$psr7 = new RoadRunner\Http\PSR7Worker($worker);
+$psr7 = new RoadRunner\Http\PSR7Worker(
+    $worker,
+    new \Nyholm\Psr7\Factory\Psr17Factory(),
+    new \Nyholm\Psr7\Factory\Psr17Factory(),
+    new \Nyholm\Psr7\Factory\Psr17Factory()
+);
 
 while ($req = $psr7->waitRequest()) {
     try {
