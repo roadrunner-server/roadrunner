c25616b937d120d16d74cfb2d12d9133afb54330
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-07-24 12:23:04 +0300

    Remove unused code
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- tests/plugins/jobs/helpers.go
+++ tests/plugins/jobs/helpers.go
@@ -81,33 +81,6 @@ func pushToPipe(pipeline string) func(t *testing.T) {
 	}
 }
 
-func pushToPipeExpectErr(pipeline string) func(t *testing.T) {
-	return func(t *testing.T) {
-		conn, err := net.Dial("tcp", "127.0.0.1:6001")
-		assert.NoError(t, err)
-		client := rpc.NewClientWithCodec(goridgeRpc.NewClientCodec(conn))
-
-		req := &jobsv1beta.PushRequest{Job: &jobsv1beta.Job{
-			Job:     "some/php/namespace",
-			Id:      "1",
-			Payload: `{"hello":"world"}`,
-			Headers: map[string]*jobsv1beta.HeaderValue{"test": {Value: []string{"test2"}}},
-			Options: &jobsv1beta.Options{
-				Priority:   1,
-				Pipeline:   pipeline,
-				Delay:      0,
-				Attempts:   0,
-				RetryDelay: 0,
-				Timeout:    0,
-			},
-		}}
-
-		er := &jobsv1beta.Empty{}
-		err = client.Call("jobs.Push", req, er)
-		require.Error(t, err)
-	}
-}
-
 func pausePipelines(pipes ...string) func(t *testing.T) {
 	return func(t *testing.T) {
 		conn, err := net.Dial("tcp", "127.0.0.1:6001")
